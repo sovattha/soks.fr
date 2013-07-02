bf5d98fc811adb7d41feebba2404048773dafb1d
Sovattha Sok <sovattha.sok@ctp.com>
2013-07-02 17:45:13 +0200

    Add and use Bootstrap (CSS framework)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 // The number of milliseconds in one day
@@ -11,8 +12,20 @@ app.set('views', __dirname + '/views');
 app.use(express.compress());
 
 // Handle GET requests on /
-app.get('/', function(req, res){res.render('index.jade');});
-app.get('/video', function(req, res){res.render('video/video.jade', {title: 'Sovattha Sok, my videos'});});
+app.get('/', function(req, res) {
+	console.log("Index");
+	res.render('index.jade');
+});
+app.get('/video', function(req, res) {
+	console.log("Video");
+	res.render('video/video.jade', {title: 'Sovattha Sok, my videos'});
+});
+app.get('/sprites.png', function(req, res) {
+	console.log("Sprites");
+    var img = fs.readFileSync('./public/sprites.png');
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.end(img, 'binary');
+});
 
 // Serve up content from public directory
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
