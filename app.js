06160de134d87979f4786f29d12a78b5f2aa62c4
Sovattha Sok <sovattha.sok@ctp.com>
2013-07-03 17:59:42 +0200

    Add 404 and 500 error pages

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.set('views', __dirname + '/views');
 // Use compress middleware to gzip content
 app.use(express.compress());
 
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions : true,
+		showStack : true
+	}));
+});
+app.configure('production', function() {
+	app.use(express.errorHandler());
+});
+
 // Routes
 app.get('/', function(req, res) {
 	res.render('index.jade', {
@@ -27,5 +37,13 @@ app.use(express.static(__dirname + '/public', {
 	maxAge : oneDay
 }));
 app.use(express.directory(__dirname + '/public'));
+// Handle 404
+app.use(function(req, res) {
+   res.render('404.jade', 404);
+});
+// Handle 500
+app.use(function(error, req, res, next) {
+   res.send('500.jade', 500);
+});
 
 app.listen(process.env.PORT || 80);
