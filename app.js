21865d4e85df74daededb48236bea5c6a89563d2
Sovattha Sok <sovattha.sok@ctp.com>
2013-07-03 16:43:16 +0200

    Formatting

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-
-// The number of milliseconds in one day
-var oneDay = 86400000;
+var express = require('express'),
+	fs = require('fs'),
+	app = express();
 
 // Set path to the views (template) directory
 app.set('views', __dirname + '/views');
@@ -11,16 +8,24 @@ app.set('views', __dirname + '/views');
 // Use compress middleware to gzip content
 app.use(express.compress());
 
-// Handle GET requests on /
+// Routes
 app.get('/', function(req, res) {
-	res.render('index.jade', {title: 'Java & Javascript web developer'});
+	res.render('index.jade', {
+		title : 'Java & Javascript web developer'
+	});
 });
 app.get('/video', function(req, res) {
-	res.render('video/video.jade', {title: 'My videos'});
+	res.render('video/video.jade', {
+		title : 'My videos'
+	});
 });
 
+// The number of milliseconds in one day
+var oneDay = 86400000;
 // Serve up content from public directory
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/public', {
+	maxAge : oneDay
+}));
 app.use(express.directory(__dirname + '/public'));
 
 app.listen(process.env.PORT || 80);
