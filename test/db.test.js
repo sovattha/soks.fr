a0e3469486b304314fd891a27ab35177c87370a5
Sovattha Sok <sovattha.sok@ctp.com>
2013-07-08 14:53:51 +0200

    Remove all test data at the end of the tests

--- test/db.test.js
+++ test/db.test.js
@@ -15,48 +15,41 @@ var envHost = process.env['MONGO_NODE_DRIVER_HOST']
   ,port = envPort != null ? envPort: Connection.DEFAULT_PORT
   ,dbName = 'website';
 
-var exchangeData = {};
+  var exchangeData = {};
+  var urlString = 'mongodb://' + user + ':' + password + '@' + host + ':' + port + '/' + dbName;
 
-suite('database', function() {
+  suite('database', function() {
   test('open should open database connection', function(done) {
-    var urlString = 'mongodb://' + user + ':' + password + '@' + host + ':' + port + '/' + dbName;
     MongoClient.connect(urlString, function(err, db) {
-      if(err) throw err;
+      if (err)
+        throw err;
 
       var collection = db.collection('test_insert');
-      collection.insert({a:2}, function(err, docs) {
+      collection.insert({
+        a : 2
+      }, function(err, docs) {
 
         collection.count(function(err, count) {
           console.log("count = %s", count);
         });
 
-        // Locate all the entries using find
         collection.find().toArray(function(err, results) {
           console.dir(results);
-          // Let's close the db
           db.close();
           done();
         });
       });
     });
   });
-//  test('insertOne should insert a transaction', function(done) {
-//    var ord = generateRandom(exchangeData);
-//    db.insertOne('transactions', ord, function(err, order) {
-//      should.not.exist(err);
-//      should.exist(order._id);
-//      insertedOrder = order;
-//      done();
-//    });
-//  });
-//  test('findOne should find a single transaction', function(done) {
-//    var id = insertedOrder._id;
-//    db.findOne('transactions', id, function(err, order) {
-//      should.not.exist(err);
-//      should.exist(order._id);
-//      order.price.should.eql(insertedOrder.price);
-//      order.volume.should.eql(insertedOrder.volume);
-//      done();
-//    });
-//  });
+  test('remove all', function(done) {
+    MongoClient.connect(urlString, function(err, db) {
+      if (err)
+        throw err;
+      var collection = db.collection('test_insert');
+      collection.remove(function() {
+        db.close();
+        done();
+      });
+    });
+  });
 });
