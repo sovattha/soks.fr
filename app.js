1ceb8a9b50a600f5360662e698f7be450394fd87
Sovattha SOK <s.sok@iemgroup.com>
2015-09-18 15:30:19 +0200

    Update to Express 4

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
-var express = require('express'),
-	fs = require('fs'),
-	router = require('./router'),
-	app = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var router = require('./router');
+var app = express();
 
-// Set path to the views (template) directory
-app.set('views', __dirname + '/views');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// Enable pretty rendering by Jade
-app.locals.pretty = true;
 
-// Use compress middleware to gzip content
-app.use(express.compress());
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
 
-app.configure('development', function() {
-	app.use(express.errorHandler({
-		dumpExceptions : true,
-		showStack : true
-	}));
-});
-app.configure('production', function() {
-	app.use(express.errorHandler());
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.get('/', router.index);
 app.get('/video', router.video);
 app.get('/traffic', router.traffic);
 
-// The number of milliseconds in one day
-var oneDay = 86400000;
-// Serve up content from public directory
-app.use(express.static(__dirname + '/public', {
-	maxAge : oneDay
-}));
-app.use(express.directory(__dirname + '/public'));
-// Handle 404
-app.use(router._404);
-// Handle 500
-app.use(router._500);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-app.listen(process.env.PORT || 80);
 
-exports.listen = function (port) {
-  app.listen(port);
-};
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('500', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('500', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
